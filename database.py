dde1a0026cd52313e70cad639934239361262bc7
Benjamin Peña <144568088+BenjaminCoderSW@users.noreply.github.com>
2025-01-26 01:22:48 -0600

    Se tuvo que añadir BD en memoria

--- database.py
+++ database.py
@@ -3,7 +3,10 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 # URL de la base de datos SQLite
-SQLALCHEMY_DATABASE_URL = "sqlite:///./taller_mecanico.db"
+# SQLALCHEMY_DATABASE_URL = "sqlite:///./taller_mecanico.db"
+
+# URL de la base de datos SQLite en memoria
+SQLALCHEMY_DATABASE_URL = "sqlite:///:memory:"
 
 # Motor de la base de datos
 engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
